69d57049bcd423eee8474127206d0d8e0e69ab48
flaviodeaquino <flavio.deaquinof@gmail.com>
2024-05-28 13:46:24 -0300

    aula 69 - segundo CRUD - Tipos de Mineração

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,30 +8,37 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-puts "Cadastrando as moedas no banco de dados..."
+spinner = TTY::Spinner.new("[:spinner] Cadastrando as moedas no banco de dados...")
+spinner.auto_spin
 
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "https://w7.pngwing.com/pngs/450/133/png-transparent-bitcoin-cryptocurrency-virtual-currency-decal-blockchain-info-bitcoin-text-trademark-logo.png"
-)
+coins = [
+            { 
+                description: "Bitcoin",
+                acronym: "BTC",
+                url_image: "https://w7.pngwing.com/pngs/450/133/png-transparent-bitcoin-cryptocurrency-virtual-currency-decal-blockchain-info-bitcoin-text-trademark-logo.png" 
+            },
 
-Coin.create!(
-    description: "Ethereum",
-    acronym: "ETH",
-    url_image: "https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png"
-)
+            { 
+                description: "Ethereum",
+                acronym: "ETH",
+                url_image: "https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png" 
+            },
 
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://w7.pngwing.com/pngs/9/37/png-transparent-dash-dashcoin-crypto-currency-and-coin-icon.png"
-)
+            { 
+                description: "Dash",
+                acronym: "DASH",
+                url_image: "https://w7.pngwing.com/pngs/9/37/png-transparent-dash-dashcoin-crypto-currency-and-coin-icon.png" 
+            },
 
-Coin.create!(
-    description: "Dogecoin",
-    acronym: "DOGE",
-    url_image: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/dogecoin-doge-icon.png"
-)
+            { 
+                description: "Dogecoin",
+                acronym: "DOGE",
+                url_image: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/dogecoin-doge-icon.png" 
+            }
+        ]
 
-puts "Moedas cadastradas no banco de dados..."
+coins.each do |coin|
+    Coin.find_or_create_by!(coin)
+end
+
+spinner.success("Moedas cadastradas no banco de dados...")
